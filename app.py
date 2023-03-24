c172902b5f78876cdf1773d129895bae2be3f44a
egor <egorchik.rog@yandex.ru>
2023-03-24 13:16:12 +0300

    seconD

--- app.py
+++ app.py
@@ -5,15 +5,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['post', 'get'])
 def index():
-    params_get = {}
-    for p in request.args:
-        params_get[p] = request.args[p]
-
     params_post = {}
+    sum = 5000000
+    proc = 50
+    term = 180
+    idx = 1
     for p in request.form:
         params_post[p] = request.form[p]
+        if idx == 1:
+            sum = int(params_post[p])*1000000
+        if idx == 2:
+            proc = int(params_post[p])
+        if idx == 3:
+            term = int(params_post[p])
+        idx += 1
 
-    return render_template('index.html', params_get=params_get, params_post=params_post)
+    
+
+    return render_template('index.html', params_post=request.form)
 
 
 if __name__ == '__main__':
